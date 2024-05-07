3ab5d789c80721a0fb9c14eb3ea087a4c08bb484
Sagar Bhatia <sagarbhatia722@gmail.com>
2024-05-07 13:58:26 +0530

    routing setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,42 @@
 import React from "react";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import AppLayout from "./layouts/app-layout";
+import Home from "./pages/home";
+import Category from "./pages/category";
+import Search from "./pages/search";
+import GifPage from "./pages/single-gif";
+import Favourites from "./pages/favourites";
+
+const router = createBrowserRouter([
+  {
+    element: <AppLayout />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/:category",
+        element: <Category />,
+      },
+      {
+        path: "/search/:query",
+        element: <Search />,
+      },
+      {
+        path: "/:type/:slug",
+        element: <GifPage />,
+      },
+      {
+        path: "/favourites",
+        element: <Favourites />,
+      },
+    ],
+  },
+]);
 
 const App = () => {
-  return <div>App</div>;
+  return <RouterProvider router={router} />;
 };
 
 export default App;
---
+++ src/pages/single-gif.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const GifPage = () => {
+  return <div>Home</div>;
+};
+
+export default GifPage;
