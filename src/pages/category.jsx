d857eb1f91be58623f2b312cf1551c73ed3092fe
Sagar Bhatia <sagarbhatia722@gmail.com>
2024-05-10 01:54:20 +0530

    category page completed

--- src/pages/category.jsx
+++ src/pages/category.jsx
@@ -1,7 +1,47 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { GifState } from "../context/gif-context";
+import Gif from "../components/gif";
 
 const Category = () => {
-  return <div>Home</div>;
+  const [results, setResults] = useState([]);
+  const { category } = useParams();
+
+  const { gf } = GifState();
+
+  const fetchCategoryResults = async () => {
+    const { data } = await gf.gifs(category, category);
+    setResults(data);
+  };
+
+  useEffect(() => {
+    fetchCategoryResults();
+  }, [category]);
+
+  return (
+    <div className="flex flex-col sm:flex-row gap-5 my-4">
+      <div className="w-full sm:w-72">
+        {results.length > 0 && <Gif gif={results[0]} />}
+        <span>Don't tell it to me, GIF it to me!</span>
+        <div className="h-0.5 bg-gray-600 my-3" />
+      </div>
+      <div>
+        <h2 className="text-4xl pb-1 font-extrabold capitalize">
+          {category.split("-").join(" & ")} GIFs
+        </h2>
+        <h2 className="text-lg text-gray-400 pb-3 font-bold hover:text-gray-50 cursor-pointer">
+          @{category}
+        </h2>
+        {results.length > 0 && (
+          <div className="columns-2 md:columns-3 lg:columns-4 xl:columns-5 gap-2">
+            {results.slice(1).map((gif) => (
+              <Gif gif={gif} key={gif.id} />
+            ))}
+          </div>
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default Category;
